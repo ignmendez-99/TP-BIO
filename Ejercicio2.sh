73e1c7ff5a9d9d779abf76f9c18c670916d35965
Guido Barbieri Pederiva <gbarbieripederiva@gmail.com>
2022-09-28 19:41:52 -0300

    finish ejercicio 2

---
+++ Ejercicio2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+printUsage(){
+    echo "Usage Ejercicio2.sh (local|remoto)";
+    exit 1
+}
+
+INPUT_FILE="output_files/Ejercicio1_1.fasta"
+OUTPUT_FILE="output_files/blast.out"
+
+if [[ $# -ne 1 ]] 
+then
+echo $#
+    printUsage;
+fi
+
+if [[ "$1" = "local" ]]
+then
+    echo "Doing a local search from $INPUT_FILE to $OUTPUT_FILE"
+    blastp -db swissprotDB -query $INPUT_FILE -out $OUTPUT_FILE -outfmt 13
+elif [[ "$1" = "remoto" ]]
+then
+    echo "Doing a renote search from $INPUT_FILE to $OUTPUT_FILE"
+    blastp -db swissprot -query $INPUT_FILE -out $OUTPUT_FILE -outfmt 13 -remote
+else 
+    printUsage;
+fi
--- Ex2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-blastp -db swissprotDB -query output_files/Ejercicio1_1.fasta -out output_files/blast.out
